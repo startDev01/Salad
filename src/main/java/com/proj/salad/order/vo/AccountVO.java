613def3b1f09ecff811c0cd75fa57fbee4036ad6
Yuri Ha <hayuri90@gmail.com>
2023-07-31 15:54:39 +0900

    .

--- src/main/java/com/proj/salad/order/vo/AccountVO.java
+++ src/main/java/com/proj/salad/order/vo/AccountVO.java
@@ -15,5 +15,73 @@ public class AccountVO {
 	private String accountCVC; // CVC
 	private String accountName; // 예금주
 	private String purchaseType; // 결제수단
-	
+
+	public int getOrderAccountNum() {
+		return OrderAccountNum;
+	}
+
+	public void setOrderAccountNum(int orderAccountNum) {
+		OrderAccountNum = orderAccountNum;
+	}
+
+	public int getOrderNum() {
+		return OrderNum;
+	}
+
+	public void setOrderNum(int orderNum) {
+		OrderNum = orderNum;
+	}
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public String getAccountPeriod() {
+		return accountPeriod;
+	}
+
+	public void setAccountPeriod(String accountPeriod) {
+		this.accountPeriod = accountPeriod;
+	}
+
+	public String getAccountCVC() {
+		return accountCVC;
+	}
+
+	public void setAccountCVC(String accountCVC) {
+		this.accountCVC = accountCVC;
+	}
+
+	public String getAccountName() {
+		return accountName;
+	}
+
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+
+	public String getPurchaseType() {
+		return purchaseType;
+	}
+
+	public void setPurchaseType(String purchaseType) {
+		this.purchaseType = purchaseType;
+	}
+
+	@Override
+	public String toString() {
+		return "AccountVO{" +
+				"OrderAccountNum=" + OrderAccountNum +
+				", OrderNum=" + OrderNum +
+				", accountNumber='" + accountNumber + '\'' +
+				", accountPeriod='" + accountPeriod + '\'' +
+				", accountCVC='" + accountCVC + '\'' +
+				", accountName='" + accountName + '\'' +
+				", purchaseType='" + purchaseType + '\'' +
+				'}';
+	}
 }
