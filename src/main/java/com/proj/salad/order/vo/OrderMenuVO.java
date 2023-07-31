d31e442b9b4191be7a12a34349de9a14488bb0f3
Yuri Ha <hayuri90@gmail.com>
2023-07-31 15:57:00 +0900

    .

--- src/main/java/com/proj/salad/order/vo/OrderMenuVO.java
+++ src/main/java/com/proj/salad/order/vo/OrderMenuVO.java
@@ -12,4 +12,55 @@ public class OrderMenuVO {
     private String userId;
     private String menuName;
     private int menuCount;
+
+    public int getOrderMenuNum() {
+        return orderMenuNum;
+    }
+
+    public void setOrderMenuNum(int orderMenuNum) {
+        this.orderMenuNum = orderMenuNum;
+    }
+
+    public int getOrderNum() {
+        return orderNum;
+    }
+
+    public void setOrderNum(int orderNum) {
+        this.orderNum = orderNum;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public void setMenuName(String menuName) {
+        this.menuName = menuName;
+    }
+
+    public int getMenuCount() {
+        return menuCount;
+    }
+
+    public void setMenuCount(int menuCount) {
+        this.menuCount = menuCount;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderMenuVO{" +
+                "orderMenuNum=" + orderMenuNum +
+                ", orderNum=" + orderNum +
+                ", userId='" + userId + '\'' +
+                ", menuName='" + menuName + '\'' +
+                ", menuCount=" + menuCount +
+                '}';
+    }
 }
